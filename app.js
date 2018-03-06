ec95ab3c07e3661e8455dba8163728dc54ac34c3
SamuliAP <samuli.anola-pukkila@hotmail.fi>
2018-03-06 14:32:44 +0200

    Added CORS to API

--- app.js
+++ app.js
@@ -1,13 +1,19 @@
-var express = require('express');
-var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
+var express      = require('express');
+var path         = require('path');
+var favicon      = require('serve-favicon');
+var logger       = require('morgan');
 var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
-
-var pizzerias = require('./routes/pizzerias');
+var bodyParser   = require('body-parser');
+var cors         = require('cors')
+var pizzerias    = require('./routes/pizzerias');
 
 var app = express();
+
+// set CORS options
+var corsOptions = {
+  origin: "http://localhost:3000",
+  optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+}
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -20,8 +26,9 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(cors(corsOptions));
 
-app.use('/§', pizzerias);
+app.use('/api', pizzerias);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
