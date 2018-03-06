4ce4b1522cd30b3f0d22ef5c6771c12e9d72dcc8
SamuliAP <samuli.anola-pukkila@hotmail.fi>
2018-03-06 14:32:00 +0200

    improved response semantics

--- controllers/pizzeriaController.js
+++ controllers/pizzeriaController.js
@@ -2,26 +2,47 @@ var Pizzeria = require('../models').Pizzeria;
 
 exports.getPizzerias = (req, res, next) => {
   Pizzeria.findAll()
-  .then(pizzerias => res.status(200).send(pizzerias))
+    .then(pizzerias => {
+      if (pizzerias) {
+        res.status(200).send({ status: "success", data: pizzerias })
+      } else {
+        res.status(404).send({ error: "not found" })
+      }
+    })
 }
 
 exports.getPizzeria = (req, res, next) => {
   Pizzeria.findById(req.params.id)
-  .then(pizzeria => res.status(200).send(pizzeria))
+    .then(pizzeria => {
+      if (pizzeria) {
+        res.status(200).send({ status: "success", data: pizzeria })
+      } else {
+        res.status(404).send({ error: "not found" })
+      }
+    })
 }
 
 exports.putPizzeria = (req, res, next) => {
   Pizzeria.upsert({
-    name: req.body.name, 
+    name: req.body.name,
     notes: req.body.notes
   }).then(success => {
-    if(success) res.status(200).send("created")
-    else res.status(200).send("updated")
+    if (success) {
+      res.status(200).send({ status: "created" })
+    } else {
+      res.status(200).send({ status: "updated" })
+    }
   })
 }
 
 exports.deletePizzeria = (req, res, next) => {
   Pizzeria.findById(req.params.id)
-  .then(pizzeria => pizzeria.destroy())
-  .then(() => res.status(200).send())
+    .then(pizzeria => pizzeria.destroy())
+    .then(deletedRows => {
+      if (deletedRows > 0) {
+        res.status(200).send({ status: "success", deleted: deletedRows })
+      } else {
+        res.status(404).send({ error: "not found" })
+      }
+    })
 }
--- routes/pizzerias.js
+++ routes/pizzerias.js
@@ -3,24 +3,12 @@ var router = express.Router();
 
 var pizzeriaController = require('../controllers/pizzeriaController');
 
-router.get(
-  '/pizzerias',
-  pizzeriaController.getPizzerias
-)
+router.get('/pizzerias', pizzeriaController.getPizzerias)
 
-router.get(
-  '/pizzerias/:id',
-  pizzeriaController.getPizzeria
-)
+router.get('/pizzerias/:id', pizzeriaController.getPizzeria)
 
-router.put(
-  '/pizzerias',
-  pizzeriaController.putPizzeria
-)
+router.put('/pizzerias', pizzeriaController.putPizzeria)
 
-router.delete(
-  '/pizzerias/:id',
-  pizzeriaController.deletePizzeria
-)
+router.delete('/pizzerias/:id', pizzeriaController.deletePizzeria)
 
 module.exports = router;
